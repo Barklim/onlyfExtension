1a3c0245eac2364c9e0492332d8c0671d416ab7b
Barklim <46284548+Barklim@users.noreply.github.c>
2023-10-12 15:07:04 +0800

    feat: stop words
    Добавлена стилизация для функционала запрещенных слов. Собрана строка в баннере инпута из оставшегося времени и запрещенного слова. После разблокировки возвращается
    введенный текст на букву раньше. При поиске запретных слов учитывается регистр. Не только если вводить запретное слово, но если его и вставить (ctrl + v), также вызывается баннер. Добавлен таймер обратного отсчета.
    issue: В силу особенности работы расширений, если не дожидаться разблокировки и перейти на другую страницу и после истечения времени таймера вернуться обратно на страницу, то ранее вводившийся текст пропадает.

--- Chrome_Extension/js/app/content.js
+++ Chrome_Extension/js/app/content.js
@@ -1,1 +1,76 @@
 console.log('content script ran');
+
+const textAreaId = 'new_post_text_input';
+const buttonClassname = 'b-chat__btn-submit';
+const stopWords = 'Anus vagina putana pedrila Shalava huilo moshonka elda'.toLowerCase();
+let previousText = '';
+let violationWord = '';
+
+let countdownInterval = null;
+
+const disable = (taId, btnClsname) => {
+    const textAreaEl = document.getElementById(taId)
+    const buttonEl = document.getElementsByClassName(btnClsname)
+
+    textAreaEl.style.height = '70px'
+    textAreaEl.setAttribute('maxlength', 1)
+    textAreaEl.style.opacity = '0.9'
+    textAreaEl.style.background = '#3caff0'
+    textAreaEl.value = `Вы ввели запретное слово: ${violationWord}! Впредь будьте аккуратнее... \nПриостановка, запуск таймера`
+    // at-attr="send_btn" or disable
+    buttonEl[0].innerHTML = 'blocked'
+    buttonEl[0].style.opacity = '0.2'
+
+    let countdown = 5;
+    countdownInterval = setInterval(() => {
+        textAreaEl.value = `Вы ввели запретное слово: ${violationWord}! Впредь будьте аккуратнее... \nДоступ через⏳ ${countdown.toString()} ...`
+        countdown--;
+        if (countdown < 0) {
+            clearInterval(countdownInterval);
+            enable(taId, btnClsname);
+        }
+    }, 1000);
+}
+const enable = (taId, btnClsname) => {
+    const textAreaEl = document.getElementById(taId)
+    const buttonEl = document.getElementsByClassName(btnClsname)
+    textAreaEl.style.height = '46px'
+    textAreaEl.setAttribute('maxlength', 20000)
+    textAreaEl.style.opacity = '1'
+    textAreaEl.style.background = '#161618'
+    textAreaEl.value = previousText;
+    buttonEl[0].innerHTML = 'send'
+    buttonEl[0].style.opacity = '1'
+}
+
+function findForbiddenWord(inputText, stopWords) {
+    const stopWordsArray = stopWords.split(' ');
+
+    for (const word of stopWordsArray) {
+        if (inputText.toLowerCase().includes(word)) {
+            return word;
+        }
+    }
+
+    return null;
+}
+
+const inputHandler = (taId, btnClsname) => {
+    const stopWord = 'nig***'
+    const textAreaEl = document.getElementById(taId)
+    const buttonEl = document.getElementsByClassName(btnClsname)
+
+    textAreaEl.addEventListener('input', function(e) {
+        const forbiddenWord = findForbiddenWord(e.target.value, stopWords);
+
+        previousText = e.target.value.slice(0, -1);
+        violationWord = forbiddenWord;
+
+        // Если найдено запрещенное слово, вызываем функцию disable
+        if (forbiddenWord) {
+            disable(textAreaId, buttonClassname);
+        }
+    });
+}
+
+setTimeout(() => inputHandler(textAreaId, buttonClassname), 4000)
